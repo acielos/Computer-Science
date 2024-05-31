0efbcef78ff6cee71e402888cc32128278ef16db
Antonio Cielos <83181287+acielos@users.noreply.github.com>
2024-05-31 09:48:08 +0200

    Delete 3-course/Representación del Conocimiento/Ejercicios Básicos/listaInversa.pl

--- 3-course/Representación del Conocimiento/Ejercicios Básicos/listaInversa.pl
+++
@@ -1,7 +0,0 @@
-
-% reverse(+List, -ListaR)
-% ListR unifica con una lista que contiene los mismos elementos que List pero en orden inverso
-
-reverse([],[]).
-
-reverse([Cab|Resto], L2) :- reverse(Resto, L), append(L, [Cab], L2).
