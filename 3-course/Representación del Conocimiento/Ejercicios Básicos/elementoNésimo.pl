219be667007c20f7abee0cb289856b57064fcbdd
Antonio Cielos <83181287+acielos@users.noreply.github.com>
2024-05-31 09:47:55 +0200

    Delete 3-course/Representación del Conocimiento/Ejercicios Básicos/elementoNésimo.pl

--- 3-course/Representación del Conocimiento/Ejercicios Básicos/elementoNésimo.pl
+++
@@ -1,8 +0,0 @@
-
-% elem_enesimo(+Pos, +Lista, -Elem)
-% Es cierto cuando Elem unifica con el elemento que ocupa la posición Pos
-% dentro de Lista.
-
-elem_enesimo(1, [Cab|_], Cab).
-elem_enesimo(N, [Cab|Resto], E) :- N > 1, N2 is N - 1, elem_enesimo(N2, Resto, Elem).
-
