a6bb573b9cd98efdfb5b3c41ff5a1a3eda3851ae
acielos <antoniocielos@gmail.com>
2025-05-28 18:42:03 +0200

    modified:   "2-course/[APSO] Administraci\303\263n y Programaci\303\263n de los Sistemas Operativos/2\302\252 Parte/Pr\303\241ctica 3/3.2/A.c"

--- 2-course/[APSO] Administración y Programación de los Sistemas Operativos/2ª Parte/Práctica 3/3.2/A.c
+++ 2-course/[APSO] Administración y Programación de los Sistemas Operativos/2ª Parte/Práctica 3/3.2/A.c
@@ -5,7 +5,27 @@
 
 int main(){
 
-    
+    // Variable para guardar el PID del proceso B
+    int pidB;
+
+    // Mostramos mensaje de bienvenida
+    printf("Bienvenid@ al proceso A!\n");
+
+    // Lanzamos el proceso B
+    pidB = fork();
+    if (pidB == 0)
+    {
+        execl("B", "B", NULL);
+        perror("Error en el EXECL del proceso B\n");
+        exit(-1);
+    }
+
+    // Mostramos PID's de los procesos A y B
+    printf("PID del proceso A: %d\n", getpid());
+    printf("PID del proceso B: %d\n", pidB);
+
+    // Esperamos un segundo
+    sleep(1);
 
     // Finalizamos el programa
     return 0;
