51e2297a65d4e93ab19cebed04e1da318fa41298
acielos <antoniocielos@gmail.com>
2025-06-06 17:42:22 +0200

    v0

---
+++ 2-course/[APSO] Administración y Programación de los Sistemas Operativos/2ª Parte/Práctica 5/5.2/A.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main(){
+
+    // Variable para PID del proceso B
+    int pidB;
+
+    /*
+        En la PIPE hay dos "extremos", PIPE[0] y PIPE[1], de esta manera cada
+        uno de ellos sirve para una cosa, por lo que daremos a un proceso acceso
+        a uno y a otro proceso acceso a otro, dependiendo de lo que queramos conseguir:
+            - PIPE[0] -> Lectura
+            - PIPE[1] -> Escritura
+    */
+
+    // Creamos la pipe PIPE
+    int PIPE[2];
+    pipe(PIPE);
+
+    // Creamos la fifo FIFOAC
+    mkfifo("FIFOAC", 0666);
+
+    // Lanzamos el proceso B
+    pidB = fork();
+    if (pidB == 0)
+    {
+        close(0);
+        dup(PIPE[1]);
+        execl("B", "B", NULL);
+        perror("Error en el EXECL del proceso B\n");
+        exit(-1);
+    }else if (pidB == -1)
+    {
+        perror("Error en el FORK del proceso B\n");
+        exit(-1);
+    }
+
+    // Creamos los arrays para leer los números
+    int listaB[5], listaC[5];
+
+    // Abrimos la FIFO para leer
+    int fifoAC = open("FIFOAC", O_RDONLY);
+
+    // Leemos los números dados por los procesos B y C
+    for (int i = 0; i < 5; i++)
+    {
+        read(PIPE[0], &listaB[i], sizeof(listaB));
+        printf("Número %d, mandado por el proceso B\n", listaB[i]);
+        read(fifoAC, &listaC[i], sizeof(listaC));
+        printf("Número %d, mandado por el proceso C\n", listaC[i]);
+    }
+
+    // Declaramos variables para guardar las medias
+    int mediaB = 0, mediaC = 0;
+
+    // Calculamos la suma de cada array
+    for (int i = 0; i < 5; i++)
+    {
+        mediaB += listaB[i];
+        mediaC += listaC[i];
+    }
+
+    // Calculamos la media de cada array
+    mediaB /= 5;
+    mediaC /= 5;
+
+    // Comparamos cuál es mayor y lo comunicamos a B
+    if (mediaB > mediaC)
+    {
+        kill(pidB, 10);
+    }else
+    {
+        kill(pidB, 12);
+    }
+
+    // Esperamos a que finalice el proceso B
+    wait(NULL);
+
+    // Mensaje de finalización
+    printf("Proceso A finalizado...\n");
+
+    // Finalizamos el proceso A
+    return 0;
+}
---
+++ 2-course/[APSO] Administración y Programación de los Sistemas Operativos/2ª Parte/Práctica 5/5.2/C.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+// Variable y funciones para esperar señal de resultado
+resultado = 0;
+void setWinner(){ resultado = 1; }
+void setLoser(){ resultado = 2; }
+
+int main(){
+
+    // Escuchamos las señales...
+    signal(10, setWinner);
+    signal(12, setLoser);
+
+    // Variable para guardar el PID del proceso C
+    int pidC;
+
+    // Abrimos la PIPE
+    int pipeAB = dup(2);
+
+    // Lanzamos el proceso C
+    pidC = fork();
+    if (pidC == 0)
+    {
+        execl("C", "C", NULL);
+        perror("    Error en el EXECL del proceso C\n");
+        exit(-1);
+    }else if (pidC == -1)
+    {
+        perror("    Error en el FORK del proceso C\n");
+        exit(-1);
+    }
+
+    // Preparamos la semilla para generar números aleatorios
+    srand(getpid());
+
+    // Variable para guardar los números aleatorios
+    int ale;
+
+    // Generamos y escribimos los 5 números aleatorios
+    for (int i = 0; i < 5; i++)
+    {
+        ale = rand() % 5 + 1;
+        write(pipeAB, &ale, sizeof(ale));
+    }
+
+    // Esperamos a que nos informen del resultado
+    while (resultado == 0)
+    {
+        pause();
+    }
+
+    if (resultado == 1)
+    {
+        kill(pidC, 10);
+    }else if (resultado == 2)
+    {
+        kill(pidC, 12);
+    }
+
+    // Esperamos a que C termine...
+    wait(NULL);
+
+    // Mensaje de finalización
+    printf("    Proceso B finalizado...\n");
+    
+    // Finalizamos el proceso B
+    return 0;
+}
