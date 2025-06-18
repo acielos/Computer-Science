efbb25ae6662372d6f2a3d14be9700219562be4e
acielos <antoniocielos@gmail.com>
2025-06-18 13:51:46 +0200

    v1.0

---
+++ 2-course/[APSO] Administración y Programación de los Sistemas Operativos/2ª Parte/2A/Práctica 6/6.2/C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/msg.h>
+
+int main(){
+
+    
+    
+    // Finalizamos el proceso C
+    return 0;
+}
